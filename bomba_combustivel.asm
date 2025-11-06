bd50b67b5aab3b0a0d59a1da0961aaf120751a39
enzo.kretzer <kretzer.enzo60@gmail.com>
2025-11-06 14:24:26 -0300

    getDoubleValue implementado

--- bomba_combustivel.asm
+++ bomba_combustivel.asm
@@ -1,5 +1,6 @@
 .data
 	zeroD:			.double 0
+	cemD:			.double 100
 	precoComum: 		.double 6.14
 	precoAditivada:		.double 6.34
 	precoAlcool:		.double 4.13
@@ -21,9 +22,9 @@
 .text
 
 main:
-	jal	getFormaPagamento
-	move	$a0, $v0
-	li	$v0, 1
+	jal	getDoubleValue
+	mov.d	$f12, $f0
+	li	$v0, 3
 	syscall
 
 	li	$v0, 10
@@ -176,3 +177,60 @@ getFormaPagamento:
 	lw	$ra, 0($sp)
 	addi	$sp, $sp, 4
 	jr	$ra
+	
+# $f0 -> retorna um valor 
+getDoubleValue:
+	addi	$sp, $sp -4
+	sw	$ra, 0($sp)
+	
+	move	$s0, $zero
+	getValueLoop:
+		validValueLoop:
+			addi	$sp, $sp -4
+			sw	$s0, 0($sp)
+			
+			jal	getHexKeyboardInput
+			move	$t1, $v0
+			
+			lw	$s0, 0($sp)
+			addi	$sp, $sp, 4
+			
+			bnez	$s0, cont		# Apenas na primeira iteração
+			beqz	$t1, validValueLoop	# Não adiciona zeros à esquerda
+			cont:
+			sgt	$t2, $t1, 10
+			bnez	$t2, validPagamentoLoop
+			
+		beq	$t1, 10, endValueLoop
+		addi	$sp, $sp, -4
+		sw	$t1, 0($sp)
+		addi	$s0, $s0, 1
+		j	getValueLoop
+		
+	endValueLoop:
+		slti	$t5, $s0, 2
+		bnez	$t5, getValueLoop
+		move	$t1, $zero	#iterador
+		li	$t2, 1		# multiplicador
+	translateValue:
+		lw	$t3, 0($sp)
+		mul	$t3, $t3, $t2
+		add	$t4, $t4, $t3
+		addi	$t1, $t1, 1
+		addi	$sp, $sp, 4
+		mul	$t2, $t2, 10
+		bne	$t1, $s0, translateValue
+		
+	mtc1	$t4, $f4
+	cvt.d.w	$f4, $f4
+	l.d	$f6, cemD
+	div.d	$f4, $f4, $f6
+	mov.d	$f0, $f4
+	
+	lw	$ra, 0($sp)
+	addi	$sp, $sp, 4
+	jr	$ra
+	
+# $a0 -> forma de pagamento
+# $f0 -> preco do combustivel escolhido
+pagarCombustivel:
