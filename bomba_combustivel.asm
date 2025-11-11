553293169073075422a23803e05726b10e93314b
enzo.kretzer <kretzer.enzo60@gmail.com>
2025-11-10 22:28:32 -0300

    Proc de escrita e código da main

--- bomba_combustivel.asm
+++ bomba_combustivel.asm
@@ -13,6 +13,8 @@
 		.byte	0x18,0x28,0x48,0x88
 		
 	newline:	.asciiz "\n"
+	iniciando:	.asciiz "=== INICIANDO SISTEMA DE BOMBA DE COMBUSTÍVEL ===\n\n"
+	finalizando:	.asciiz "== FINALIZANDO SISTEMA DE BOMBA DE COMBUSTÍVEL ==\n"
 	getCombM1:	.asciiz "Indique o Combustível desejado:\n"
 	getCombM2:	.asciiz "1 - Gasolina Comum R$"
 	getCombM3:	.asciiz "2 - Gasolina Aditivada R$"
@@ -24,21 +26,132 @@
 	ChangeCombM5:	.asciiz "Insira o novo valor para Álcool: "
 	ChangeCombM6:	.asciiz "Valores alterados!\n\n"
 	getPagM1:	.asciiz "Pagar combustível por:\n"
-	getPagM2:	.asciiz "1 - Valor monetário \n"
-	getPagM3:	.asciiz "2 - Litro \n"
+	getPagM2:	.asciiz "1 - Valor monetário\n"
+	getPagM3:	.asciiz "2 - Litro\n\n"
+	monetarioM:	.asciiz "Digite o valor pago (Duas casas após vírgula)\n"
+	litrosM:	.asciiz "Digite quantos litros deseja encher\n"
 	encherM:	.asciiz "Enchendo o tanque...\n"
-	enchidoM:	.asciiz "Tanque enchido!\n"
-	finalizaM:	.asciiz "Finalizar sistema?\n"
+	enchidoM:	.asciiz "Tanque enchido!\n\n"
+	finalizaM:	.asciiz "Continuar rodando sistema?\n"
 	simOptM:	.asciiz "1 - Sim\n"
 	naoOptM:	.asciiz "2 - Não\n"
+	
+	notaBuffer:	.space 32
+	notaNumBuffer:	.space 12
+	notaTitleBuffer:.space 20
+	notaPrecoBuffer:.space 64
+	notaPrefix:	.asciiz "NotaFiscal-"
+	notaSufix:	.asciiz ".txt"
+	notaCarro:	.asciiz "CARRO "
+	notaPrecoStart:	.asciiz "Total: "
+	comb1:		.asciiz "Combustível: Gasolina Comum\n"
+	comb2:		.asciiz "Combustível: Gasolina Aditivada\n"
+	comb3:		.asciiz "Combustível: Álcool\n"
+	comma:		.asciiz ","
+	dot:		.asciiz "."
 .text
 
 main:
-	jal	getContinuaSist
-	move	$a0, $v0
-	li	$v0, 1
+	li	$v0, 4
+	la	$a0, iniciando
 	syscall
 
+	li	$s0, 1	# Iteração
+	li	$s1, 1	# Sinal de continua
+	main_while:
+		bne	$s1, 1, main_while_end
+		
+		addi	$sp, $sp, -8
+		sw	$s1, 4($sp)
+		sw	$s0, 0($sp)
+		
+		jal	getCombustivel
+		move	$s2, $v0	# Combustivel solicitado
+		
+		lw	$s0, 0($sp)
+		lw	$s1, 4($sp)
+		addi	$sp, $sp, 8
+		
+		addi	$sp, $sp, -12
+		sw	$s2, 8($sp)
+		sw	$s1, 4($sp)
+		sw	$s0, 0($sp)
+		
+		jal	getFormaPagamento
+		move	$s3, $v0	# Forma de pagamento
+		
+		lw	$s0, 0($sp)
+		lw	$s1, 4($sp)
+		lw	$s2, 8($sp)
+		addi	$sp, $sp, 12
+		
+		addi	$sp, $sp, -16
+		sw	$s3, 12($sp)
+		sw	$s2, 8($sp)
+		sw	$s1, 4($sp)
+		sw	$s0, 0($sp)
+		
+		move	$a0, $s2
+		jal	getPrecoLitro
+		mov.d	$f20, $f0	# Preco do litro
+		
+		lw	$s0, 0($sp)
+		lw	$s1, 4($sp)
+		lw	$s2, 8($sp)
+		lw	$s3, 12($sp)
+		addi	$sp, $sp, 16
+		
+		addi	$sp, $sp, -16
+		sw	$s3, 12($sp)
+		sw	$s2, 8($sp)
+		sw	$s1, 4($sp)
+		sw	$s0, 0($sp)
+		
+		move	$a0, $s3
+		mov.d	$f12, $f20
+		jal	pagarCombustivel
+		mov.d	$f22, $f0
+		
+		lw	$s0, 0($sp)
+		lw	$s1, 4($sp)
+		lw	$s2, 8($sp)
+		lw	$s3, 12($sp)
+		addi	$sp, $sp, 16
+		
+		addi	$sp, $sp, -16
+		sw	$s3, 12($sp)
+		sw	$s2, 8($sp)
+		sw	$s1, 4($sp)
+		sw	$s0, 0($sp)
+		
+		move	$a0, $s0
+		move	$a1, $s2
+		mov.d	$f12, $f22
+		jal	emitirNotaFiscal
+		
+		lw	$s0, 0($sp)
+		lw	$s1, 4($sp)
+		lw	$s2, 8($sp)
+		lw	$s3, 12($sp)
+		addi	$sp, $sp, 16
+		
+		addi	$sp, $sp, -4
+		sw	$s0, 0($sp)
+		
+		jal	getContinuaSist
+		move	$s1, $v0
+		
+		lw	$s0, 0($sp)
+		addi	$sp, $sp, 4
+		
+		addi	$s0, $s0, 1
+		j	main_while
+	main_while_end:
+	
+	li	$v0, 4
+	la	$a0, finalizando
+	syscall
+		
 	li	$v0, 10
 	syscall
 	
@@ -148,6 +261,10 @@ getCombustivel:
 	la	$a0, newline
 	li	$v0, 4
 	syscall
+	
+	la	$a0, newline
+	li	$v0, 4
+	syscall
 	validCombustivelLoop:
 		jal	getHexKeyboardInput
 		move	$t0, $v0
@@ -179,6 +296,9 @@ updatePrecoCombustivel:
 	li	$v0, 4
 	syscall
 	
+	la	$a0, newline
+	li	$v0, 4
+	syscall
 	validPrecoCombustivelOpt:
 		jal	getHexKeyboardInput
 		move	$t0, $v0
@@ -412,6 +532,11 @@ pagarCombustivel:
 	
 	IF_PAG:
 	bne	$a0, 1, ELSE_PAG
+	
+	la	$a0, monetarioM
+	li	$v0, 4
+	syscall
+	
 	jal	getDoubleValue
 	mov.d	$f4, $f0
 	div.d	$f6, $f4, $f12
@@ -424,6 +549,11 @@ pagarCombustivel:
 	j	END_IF_PAG
 	
 	ELSE_PAG:
+	
+	la	$a0, litrosM
+	li	$v0, 4
+	syscall
+	
 	jal	getIntValue
 	move	$s0, $v0
 	mul	$t0, $s0, 1000
@@ -455,6 +585,10 @@ getContinuaSist:
 	la	$a0, naoOptM
 	li	$v0, 4
 	syscall
+	
+	la	$a0, newline
+	li	$v0, 4
+	syscall
 	validFinalizaOpt:
 		jal	getHexKeyboardInput
 		move	$t0, $v0
@@ -466,4 +600,248 @@ getContinuaSist:
 	
 	lw	$ra, 0($sp)
 	addi	$sp, $sp, 4
+	jr	$ra
+
+# $a0  -> Iteração do loop principal (número do carro)
+# $a1  -> Tipo de combustível
+# $f12 -> Preço pago
+emitirNotaFiscal:
+	la	$s0, notaNumBuffer
+	move	$t0, $a0
+	li	$t1, 10
+	move	$t4, $zero
+	
+	itoa_push:
+		div	$t0, $t1
+		mflo	$t0
+		mfhi	$t2
+		addi	$t2, $t2, 48
+		
+		addi	$sp, $sp, -4
+		sw	$t2, 0($sp)
+		addi	$t4, $t4, 1
+		
+		bnez	$t0, itoa_push
+	
+	itoa_pop:
+		beqz	$t4, itoa_end
+		
+		lw	$t2, 0($sp)
+		addi	$sp, $sp, 4
+		
+		sb	$t2, 0($s0)
+		addi	$s0, $s0, 1
+		addi	$t4, $t4, -1
+		j	itoa_pop
+	itoa_end:
+	
+	la	$s0, notaBuffer
+	la	$t0, notaPrefix
+	prefixLoop:
+		lb	$t1, 0($t0)
+		beqz	$t1, prefixLoopEnd
+		sb	$t1, 0($s0)
+		addi	$t0, $t0, 1
+		addi	$s0, $s0, 1
+		j	prefixLoop
+	prefixLoopEnd:
+	
+	la	$t0, notaNumBuffer
+	itLoop:
+		lb	$t1, 0($t0)
+		beqz	$t1, itLoopEnd
+		sb	$t1, 0($s0)
+		addi	$t0, $t0, 1
+		addi	$s0, $s0, 1
+		j	itLoop
+	itLoopEnd:
+	
+	la	$t0, notaSufix
+	sufixLoop:
+		lb	$t1, 0($t0)
+		sb	$t1, 0($s0)
+		addi	$s0, $s0, 1
+		addi	$t0, $t0, 1
+		bnez	$t1, sufixLoop
+		
+	addi	$sp, $sp, 4
+	sw	$a1, 0($sp)
+		
+	li	$v0, 13
+	la	$a0, notaBuffer
+	li	$a1, 1
+	li	$a2, 0
+	syscall
+	move	$s1, $v0
+	
+	la	$s0, notaTitleBuffer
+	la	$t0, notaCarro
+	move	$t3, $zero
+	titleStartLoop:
+		lb	$t1, 0($t0)
+		beqz	$t1, titleStartLoopEnd
+		sb	$t1, 0($s0)
+		addi	$t0, $t0, 1
+		addi	$s0, $s0, 1
+		addi	$t3, $t3, 1
+		j	titleStartLoop
+	titleStartLoopEnd:
+	
+	la	$t0, notaNumBuffer
+	titleMiddleLoop:
+		lb	$t1, 0($t0)
+		beqz	$t1, titleMiddleLoopEnd
+		sb	$t1, 0($s0)
+		addi	$t0, $t0, 1
+		addi	$s0, $s0, 1
+		addi	$t3, $t3, 1
+		j	titleMiddleLoop
+	titleMiddleLoopEnd:
+	
+	la	$t0, newline
+	lb	$t1, 0($t0)
+	sb	$t1, 0($s0)
+	addi	$s0, $s0, 1
+	addi	$t3, $t3, 1
+
+		
+	li	$v0, 15
+	move	$a0, $s1
+	la	$a1, notaTitleBuffer
+	move	$a2, $t3
+	syscall
+	
+	lw	$a2, 0($sp)
+	addi	$sp, $sp, 4
+	
+	beq	$a2, 1, IF_COMB_COMUM
+	beq	$a2, 2, IF_COMB_COMUM
+	beq	$a2, 3, IF_COMB_ALCOOL
+	
+	IF_COMB_COMUM:
+		la	$a1, comb1
+		li	$a2, 28
+	IF_COMB_ADITIVADA:
+		la	$a1, comb2
+		li	$a2, 32
+	IF_COMB_ALCOOL:
+		la	$a1, comb3
+		li	$a2, 20
+	END_IF_COMB:
+	
+	li	$v0, 15
+	move	$a0, $s1
+	syscall
+	
+	la	$s0, notaPrecoBuffer
+	move	$t3, $zero
+	
+	la	$t0, notaPrecoStart
+	precoLoop:
+		lb	$t1, 0($t0)
+		beqz	$t1, precoLoopEnd
+		sb	$t1, 0($s0)
+		addi	$t0, $t0, 1
+		addi	$s0, $s0, 1
+		addi	$t3, $t3, 1
+		j	precoLoop
+	precoLoopEnd:
+		
+
+	trunc.w.d	$f4, $f12
+	mfc1		$t5, $f4
+		
+	cvt.d.w		$f4, $f4
+	sub.d		$f6, $f12, $f4
+	l.d		$f4, cemD
+	mul.d		$f6, $f6, $f4
+	round.w.d	$f4, $f6
+	mfc1		$t6, $f4	
+	
+	move	$t0, $t5
+	li	$t1, 10
+	move	$t4, $zero
+	ftoa_int_push:
+		div	$t0, $t1
+		mflo	$t0
+		mfhi	$t2
+		addi	$t2, $t2, 48
+		
+		addi	$sp, $sp, -4
+		sw	$t2, 0($sp)
+		addi	$t4, $t4, 1
+		
+		bnez	$t0, ftoa_int_push
+	
+	rem	$t7, $t4, 3
+	bnez	$t7, ftoa_int_pop
+	li	$t7, 3
+	ftoa_int_pop:
+		beqz	$t4, ftoa_int_end
+		
+		lw	$t2, 0($sp)
+		addi	$sp, $sp, 4
+		
+		sb	$t2, 0($s0)
+		addi	$s0, $s0, 1
+		addi	$t4, $t4, -1
+		addi	$t3, $t3, 1
+		addi	$t7, $t7, -1
+		bnez	$t7, ftoa_int_pop
+		beqz	$t4, ftoa_int_end
+		la	$t7, dot
+		lb	$t2, 0($t7)
+		sb	$t2, 0($s0)
+		addi	$s0, $s0, 1
+		addi	$t3, $t3, 1
+		li	$t7, 3
+		j	ftoa_int_pop
+	ftoa_int_end:
+	
+	la	$t0, comma
+	lb	$t2, 0($t0)
+	sb	$t2, 0($s0)
+	addi	$s0, $s0, 1
+	addi	$t3, $t3, 1
+	
+	move	$t0, $t6
+	li	$t1, 10
+	move	$t4, $zero
+	ftoa_dec_push:
+		div	$t0, $t1
+		mflo	$t0
+		mfhi	$t2
+		addi	$t2, $t2, 48
+		
+		addi	$sp, $sp, -4
+		sw	$t2, 0($sp)
+		addi	$t4, $t4, 1
+		
+		bnez	$t0, ftoa_dec_push
+	
+	move	$t6, $zero
+	ftoa_dec_pop:
+		beqz	$t4, ftoa_dec_end
+
+		lw	$t2, 0($sp)
+		addi	$sp, $sp, 4
+		
+		sb	$t2, 0($s0)
+		addi	$s0, $s0, 1
+		addi	$t4, $t4, -1
+		addi	$t3, $t3, 1
+		addi	$t6, $t6, 1
+		j	ftoa_dec_pop
+	ftoa_dec_end:
+		
+	li	$v0, 15
+	move	$a0, $s1
+	la	$a1, notaPrecoBuffer
+	move	$a2, $t3
+	syscall
+	
+	li	$v0, 16
+	move	$a0, $s1
+	syscall
+		
 	jr	$ra
